e71265ce3355b6057dcb4997bfc310a8199da652
Ulhve <ulhve@mail.ru>
2023-06-14 12:43:18 +0300

    added  interface FormulasFileChangedListener

--- src/main/java/com/example/calculator/events/FormulasFileChangedListener.java
+++ src/main/java/com/example/calculator/events/FormulasFileChangedListener.java
@@ -1,32 +1,11 @@
 package com.example.calculator.events;
 
-import com.example.calculator.model.MathFunctionWithExpression;
-import com.example.calculator.service.InputService;
-import com.example.calculator.service.FormulasParser;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import com.example.calculator.model.MathFunctionWithExpression;
+
 import java.util.List;
 
-@Component
-@RequiredArgsConstructor
-@Getter
-public class FormulasFileChangedListener {
+public interface FormulasFileChangedListener {
 
-    private List<MathFunctionWithExpression> currentFormulas = new ArrayList<>();
-
-    private final InputService fileReader;
-    private final FormulasParser formulasParser;
-
-    @EventListener
-    public void onFormulasFileChangedEvent(FormulasFileChangedEvent event){
-        SetFormulasFromFile(event.getPath());
-    }
-
-    private void SetFormulasFromFile(String path){
-        currentFormulas = formulasParser.parseFormulas(fileReader.readStrings(path));
-    }
+    List<MathFunctionWithExpression> getCurrentFormulas();
 }
---
+++ src/main/java/com/example/calculator/events/FormulasFileChangedListenerImpl.java
@@ -0,0 +1,32 @@
+package com.example.calculator.events;
+
+import com.example.calculator.model.MathFunctionWithExpression;
+import com.example.calculator.service.InputService;
+import com.example.calculator.service.FormulasParser;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Getter
+public class FormulasFileChangedListenerImpl implements FormulasFileChangedListener {
+
+    private List<MathFunctionWithExpression> currentFormulas = new ArrayList<>();
+
+    private final InputService fileReader;
+    private final FormulasParser formulasParser;
+
+    @EventListener
+    public void onFormulasFileChangedEvent(FormulasFileChangedEvent event){
+        SetFormulasFromFile(event.getPath());
+    }
+
+    private void SetFormulasFromFile(String path){
+        currentFormulas = formulasParser.parseFormulas(fileReader.readStrings(path));
+    }
+}
