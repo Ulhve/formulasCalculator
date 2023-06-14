17d137f2adacd364a7c88aa61a1889a3e8205bdc
Ulhve <ulhve@mail.ru>
2023-06-14 13:01:33 +0300

    DTOs moved to such folder

--- src/main/java/com/example/calculator/model/MathFunctionWithExpression.java
+++
@@ -1,3 +0,0 @@
-package com.example.calculator.model;
-
-public record MathFunctionWithExpression(String expression, MathFunction function) {}
--- src/main/java/com/example/calculator/model/Result.java
+++
@@ -1,5 +0,0 @@
-package com.example.calculator.model;
-
-import java.util.Map;
-
-public record Result(String expression, Map<String, Double> arguments, Double calculated) {}
